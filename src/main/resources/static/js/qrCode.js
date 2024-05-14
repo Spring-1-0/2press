8663bf48a34d309167413e6c6b0829e0e1842bbf
Daniel Panyin Mensah <78688545+Daniel024419@users.noreply.github.com>
2024-05-14 11:27:26 +0000

    Update qrCode.js

--- src/main/resources/static/js/qrCode.js
+++ src/main/resources/static/js/qrCode.js
@@ -12,22 +12,46 @@ var baseUrl = window.location.origin;
 // Generate the QR code with the concatenated URL
 qrcode.makeCode(baseUrl + "/upload");
 
+// function printQRCode() {
+//    // Create an iframe
+//    var iframe = document.createElement('iframe');
+//    iframe.style.display = 'none';
+//    document.body.appendChild(iframe);
+
+//    // Write the QR code HTML content to the iframe
+//    var qrCodeContent = document.querySelector('.qrcode').outerHTML;
+//    var iframeDoc = iframe.contentWindow.document;
+//    iframeDoc.write('<html><head><title>PrintFlow</title><style>@media print { .qrcode { position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); } }</style></head><body><h1>Scan QR Code to transfer Files</h1>' + qrCodeContent + '</body></html>');
+
+//    // Print the iframe content
+//    iframe.contentWindow.print();
+
+//    // Remove the iframe after printing
+//    document.body.removeChild(iframe);
+// }
+
+
 function printQRCode() {
-   // Create an iframe
-   var iframe = document.createElement('iframe');
-   iframe.style.display = 'none';
-   document.body.appendChild(iframe);
+  // Create an iframe
+  var iframe = document.createElement("iframe");
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
 
-   // Write the QR code HTML content to the iframe
-   var qrCodeContent = document.querySelector('.qrcode').outerHTML;
-   var iframeDoc = iframe.contentWindow.document;
-   iframeDoc.write('<html><head><title>PrintFlow</title><style>@media print { .qrcode { position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); } }</style></head><body><h1>Scan QR Code to transfer Files</h1>' + qrCodeContent + '</body></html>');
+  // Write the QR code HTML content to the iframe
+  var qrCodeContent = document.querySelector(".qrcode").outerHTML;
+  var htmlContent =
+    "<html><head><title>PrintFlow</title><style>@media print { .qrcode { position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); } }</style></head><body><h1>Scan QR Code to transfer Files</h1>" +
+    qrCodeContent +
+    "</body></html>";
+  iframe.srcdoc = htmlContent;
 
-   // Print the iframe content
-   iframe.contentWindow.print();
-
-   // Remove the iframe after printing
-   document.body.removeChild(iframe);
+  // Wait for iframe to load before printing
+  iframe.onload = function () {
+    // Print the iframe content
+    iframe.contentWindow.print();
+    // Remove the iframe after printing
+    document.body.removeChild(iframe);
+  };
 }
 
 
